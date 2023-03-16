37f3bca475052138545992f2499344f0f9324fab
hdangnguyen <hdangprod@gmail.com>
2023-03-16 10:40:50 +0700

    refactor code

--- src/page/flightDetail/index.js
+++ src/page/flightDetail/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
+import { Navbar } from '../../components'
+
 function FlightDetail() {
-  return <div>FlightDetail</div>
+  return (
+    <div className="bg-surface h-screen">
+      <Navbar />
+      <div className="grid grid-cols-12 w-4/5 mx-auto gap-6">
+        <div className="col-span-7 w-full bg-white rounded-xl ">Filter</div>
+        <div className="col-span-5 w-full bg-white rounded-xl ">Filter</div>
+      </div>
+    </div>
+  )
 }
 
 export default FlightDetail
