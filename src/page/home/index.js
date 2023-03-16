bfa80bdee14f32c9b53995e3edf412fb32257726
cnhh <cnhh85@gmail.com>
2023-03-16 11:30:05 +0700

    add dropdowns

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,9 +1,22 @@
 import { React, useState } from 'react'
 
 import BackGround from '../../assets/img/homeBg.png'
+import SearchIcon from '../../assets/svg/search-outline.svg'
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpenDeparture, setIsOpenDeparture] = useState(false)
+  const [isOpenArrival, setIsOpenArrival] = useState(false)
+  const [isOpenDate, setIsOpenDate] = useState(false)
+
+  const toggleDeparture = () => {
+    setIsOpenDeparture(!isOpenDeparture)
+  }
+  const toggleArrival = () => {
+    setIsOpenArrival(!isOpenArrival)
+  }
+  const toggleDate = () => {
+    setIsOpenDate(!isOpenDate)
+  }
   return (
     <div>
       <div className="mt-20 relative ">
@@ -11,77 +24,228 @@ const Home = () => {
           Let’s explore the world
         </h2>
         <img src={BackGround} alt="" className="w-screen" />
-        <div className="absolute w-full top-[90%] flex">
-          <div className="w-[1024px] h-[200px] bg-white mx-auto p-9 rounded shadow-sm">
+        <div className="absolute w-full top-[80%] flex">
+          <div className="w-[1024px] h-[240px] bg-white mx-auto p-9 rounded shadow-sm">
             <h3 className="text-[#1A1C1E] font-[400] text-[24px]">Search Flight</h3>
             <p className="text-[14px] font-[400] text-[#72787E]">
               Find your flight by choosing your destination
             </p>
             <div className="flex mt-6">
-              <button
-                id="dropdownDefaultButton"
-                data-dropdown-toggle="dropdown"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-              >
-                Dropdown button{' '}
-                <svg
-                  className="w-4 h-4 ml-2"
-                  aria-hidden="true"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="flex rounded-xl bg-[#EDEDED] mr-4">
+                <button
+                  className="px-5 py-2 cursor-pointer text-left relative"
+                  onClick={toggleDeparture}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M19 9l-7 7-7-7"
-                  ></path>
-                </svg>
-              </button>
-              <div
-                id="dropdown"
-                className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-              >
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownDefaultButton"
+                  <h5 className="text-[#72787E] text-[14px] font-semibold">Departure</h5>
+                  <div className="inline-block text-left">
+                    <div>
+                      <div
+                        className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-[#EDEDED] py-2 text-[16px] font-regular text-[#1A1C1E] hover:bg-[#EDEDED]/30"
+                        id="menu-button"
+                        aria-expanded="true"
+                        aria-haspopup="true"
+                      >
+                        Select your destination
+                        <svg
+                          className="-mr-1 h-5 w-5 text-gray-400"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+
+                    <div
+                      className={`absolute right-0 mt-3 w-56 origin-top-right rounded-md bg-[#ededed] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                        isOpenDeparture ? 'block' : 'hidden'
+                      }`}
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="menu-button"
+                      tabIndex="-1"
+                    >
+                      <div className="py-1" role="none">
+                        <div
+                          href="#"
+                          className="text-gray-700 z-10 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-0"
+                        >
+                          Ha Noi
+                        </div>
+                        <div
+                          href="#"
+                          className="text-gray-700 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-1"
+                        >
+                          Ha Noi
+                        </div>
+                        <div
+                          href="#"
+                          className="text-gray-700 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-2"
+                        >
+                          Ha Noi
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </button>
+
+                <div className="border border-y-0 mx-2"></div>
+
+                <button
+                  className="px-5 py-2 cursor-pointer text-left relative"
+                  onClick={toggleArrival}
                 >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  <h5 className="text-[#72787E] text-[14px] font-semibold">Arrival</h5>
+                  <div className="inline-block text-left">
+                    <div>
+                      <div
+                        className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-[#EDEDED] py-2 text-[16px] font-regular text-[#1A1C1E] hover:bg-[#EDEDED]/30"
+                        id="menu-button"
+                        aria-expanded="true"
+                        aria-haspopup="true"
+                      >
+                        Select your destination
+                        <svg
+                          className="-mr-1 h-5 w-5 text-gray-400"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+
+                    <div
+                      className={`absolute right-0 mt-3 w-56 origin-top-right rounded-md bg-[#ededed] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                        isOpenArrival ? 'block' : 'hidden'
+                      }`}
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="menu-button"
+                      tabIndex="-1"
                     >
-                      Dashboard
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Earnings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Sign out
-                    </a>
-                  </li>
-                </ul>
+                      <div className="py-1" role="none">
+                        <div
+                          href="#"
+                          className="text-gray-700 z-10 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-0"
+                        >
+                          Ha Noi
+                        </div>
+                        <div
+                          href="#"
+                          className="text-gray-700 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-1"
+                        >
+                          Ha Noi
+                        </div>
+                        <div
+                          href="#"
+                          className="text-gray-700 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-2"
+                        >
+                          Ha Noi
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </button>
+              </div>
+              <div className="flex rounded-xl bg-[#EDEDED] w-[250px]">
+                <button
+                  className="px-5 py-2 cursor-pointer text-left relative w-full"
+                  onClick={toggleDate}
+                >
+                  <h5 className="text-[#72787E] text-[14px] font-semibold">Departure date</h5>
+                  <div className="inline-block text-left w-full">
+                    <div className="flex w-full justify-center gap-x-1.5 rounded-md bg-[#EDEDED] py-2 text-[16px] font-regular text-[#1A1C1E] hover:bg-[#EDEDED]/30">
+                      <div className="mr-auto">Add dates</div>
+                      <div className="ml-auto">
+                        <svg
+                          className="-mr-1  h-5 w-5 text-gray-400"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div
+                    className={`absolute right-0 mt-3 w-56 origin-top-right rounded-md bg-[#ededed] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                      isOpenDate ? 'block' : 'hidden'
+                    }`}
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="menu-button"
+                    tabIndex="-1"
+                  >
+                    <div className="py-1" role="none">
+                      <div
+                        href="#"
+                        className="text-gray-700 z-10 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                        role="menuitem"
+                        tabIndex="-1"
+                        id="menu-item-0"
+                      >
+                        Ha Noi
+                      </div>
+                      <div
+                        href="#"
+                        className="text-gray-700 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                        role="menuitem"
+                        tabIndex="-1"
+                        id="menu-item-1"
+                      >
+                        Ha Noi
+                      </div>
+                      <div
+                        href="#"
+                        className="text-gray-700 block px-4 py-2 text-sm hover:bg-[#ffffff9a]"
+                        role="menuitem"
+                        tabIndex="-1"
+                        id="menu-item-2"
+                      >
+                        Ha Noi
+                      </div>
+                    </div>
+                  </div>
+                </button>
+              </div>
+
+              <div className="flex rounded-xl">
+                <svg xmlns={SearchIcon}></svg>
               </div>
             </div>
           </div>
